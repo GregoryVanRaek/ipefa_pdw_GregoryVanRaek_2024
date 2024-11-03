db1b2b6be63fba7ef587f6b6af7165b84c75b94c
GregoryVanRaek <vanraekgregory@hotmail.com>
2024-11-03 12:57:10 +0100

    dashboard module, header + main design

--- app/src/app/feature/dashboard/router/dashboard-router/dashboard-router.component.ts
+++ app/src/app/feature/dashboard/router/dashboard-router/dashboard-router.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faArrowUpFromBracket, faChartSimple, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard-router',
   standalone: true,
   imports: [
     RouterOutlet,
+    FaIconComponent,
   ],
   templateUrl: './dashboard-router.component.html',
   styleUrl: './dashboard-router.component.scss'
 })
 export class DashboardRouterComponent {
+  username:string = "Gregory Van Raek";
+  state:string = "Student";
+  protected readonly faUser = faUser;
 
+  protected readonly faChartSimple = faChartSimple;
+  protected readonly faHouse = faHouse;
+  protected readonly faArrowUpFromBracket = faArrowUpFromBracket;
 }
